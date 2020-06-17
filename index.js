dff954335047bf9aa12adca9aa0a21369622277c
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-17 18:42:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(){
+  
+}
+function returnLastTwoDrivers(){
+  
+}
+function returnFirstTwoDrivers(){
+  
+}
+function returnFirstTwoDrivers(){
+  
+}
+function returnFirstTwoDrivers(){
+  
+}
+function returnFirstTwoDrivers(){
+  
+}
+function returnFirstTwoDrivers(){
+  
+}
+
