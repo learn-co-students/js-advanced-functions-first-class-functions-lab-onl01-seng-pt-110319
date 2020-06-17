70c5b0ef25d3b5091193b8daeac6b102f6202568
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-17 18:53:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Code your solution in this file!
 
 function returnFirstTwoDrivers(){
-  
+  const a = (function(arr, n){
+    return arr.slice(0, 2)
+  })
 }
 function returnLastTwoDrivers(){
   const a = (function(arr, n){
-    return arr.slice(0, 2)
+    return arr.slice(-1, 2)
   })
 }
 function selectingDrivers(){
