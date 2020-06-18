4bc4cae4b1b66ad2f01e6018a124ef174be601b2
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-18 00:43:50 +0000

    Done.

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const returnFirstTwoDrivers = function(drivers){
   return drivers.slice(0,2) }
   
 const returnLastTwoDrivers = function(drivers){
-  return drivers.slice(-1, 2) }
+  return drivers.slice(-2) }
   
-function selectingDrivers(){
+selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(multiplier){
+  return function (fare){
+    return fare * multiplier
+  }
   
 }
-function createFareMultiplier(){
-  
-}
-function fareDoubler(){
-  
-}
-function fareTripler(){
+const fareDoubler = createFareMultiplier(2)
+function fareTripler = createFareMultiplier(3)
   
 }
 function selectDifferentDrivers(){
