fad0e3ae2021137a1262a50aca65f65e0be66c2f
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-06-21 16:39:06 -0400

    Done.

--- index.js
+++ index.js
@@ -3,14 +3,35 @@ function returnFirstTwoDrivers(drivers) {
 	let newDriverArray = []
 	newDriverArray.push(drivers[0],drivers[1])
 	return newDriverArray
-}
+};
 
 function returnLastTwoDrivers(drivers) {
 	let newDriverArray = drivers.slice(drivers.length-2)
 	return newDriverArray
-}
+};
 
-function selectingDrivers() {
-  let driverArray = [returnFirstTwoDrivers,returnLastTwoDrivers];
-  return driverArray
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(amount) {
+	function fareQuintupler(amount) {
+		return amount*5
+	}
+	return fareQuintupler
+};
+
+function fareDoubler(amount) {
+	return amount*2
+};
+
+function fareTripler(amount) {
+	return amount*3
+};
+
+function selectDifferentDrivers(drivers, inputFunction) {
+	if(inputFunction == returnFirstTwoDrivers) {
+		return returnFirstTwoDrivers(drivers)
+	}
+	else {
+		return returnLastTwoDrivers(drivers)
+	}
 }
