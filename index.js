540325768df0c2cc63e1f41d5898dc32ab1bce33
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-06-21 15:59:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+	let newDriverArray = []
+	newDriverArray.push(drivers[0],drivers[1])
+	return newDriverArray
+}
+
+function returnLastTwoDrivers(drivers) {
+	let newDriverArray = drivers.slice(drivers.length-2)
+	return newDriverArray
+}
+
+function selectingDrivers() {
+  let driverArray = [returnFirstTwoDrivers,returnLastTwoDrivers];
+  return driverArray
+}
